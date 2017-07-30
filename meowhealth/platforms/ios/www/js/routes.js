246e9503f88006bcc866d9b0b4ec31f85abe85bb
Jackie Zhang <jackiez3895@gmail.com>
2017-07-30 09:31:55 -0700

    new FILES that works

---
+++ meowhealth/platforms/ios/www/js/routes.js
@@ -0,0 +1,47 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('page3', {
+    url: '/page3',
+    templateUrl: 'templates/page3.html',
+    controller: 'page3Ctrl'
+  })
+
+  .state('page4', {
+    url: '/page4',
+    templateUrl: 'templates/page4.html',
+    controller: 'page4Ctrl'
+  })
+
+  .state('page8', {
+    url: '/page8',
+    templateUrl: 'templates/page8.html',
+    controller: 'page8Ctrl'
+  })
+
+  .state('page9', {
+    url: '/page9',
+    templateUrl: 'templates/page9.html',
+    controller: 'page9Ctrl'
+  })
+
+  .state('page11', {
+    url: '/page11',
+    templateUrl: 'templates/page11.html',
+    controller: 'page11Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/page4')
+
+  
+
+});
---
+++ meowhealth/www/js/controllers.js
@@ -0,0 +1,104 @@
+angular.module('app.controllers', [])
+  
+.controller('menuCtrl', ['$scope', '$stateParams', '$ionicUser', '$ionicAuth', '$state', function ($scope, $stateParams, $ionicUser, $ionicAuth, $state) {
+    
+    $scope.userData = $ionicUser.details;
+
+    $scope.logout = function(){
+        $ionicAuth.logout();
+        $state.go('menu.login');
+    }
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', '$ionicUser', '$ionicAuth', '$state', function ($scope, $stateParams, $ionicUser, $ionicAuth, $state) {
+
+    $scope.data = {
+        'email': '',
+        'password': ''
+    }
+    
+    $scope.error = '';
+    
+    if ($ionicAuth.isAuthenticated()) {
+        // Make sure the user data is going to be loaded
+        $ionicUser.load().then(function() {});
+        $state.go('menu.profile'); 
+    }
+    
+    $scope.login = function(){
+        $scope.error = '';
+        $ionicAuth.login('basic', $scope.data).then(function(){
+            $state.go('menu.profile');
+        }, function(){
+            $scope.error = 'Error logging in.';
+        })
+    }
+
+}])
+   
+.controller('meowtestCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+    //**
+    $scope.mapLink=function(){
+        //var RTM = require('..');
+    
+        var endpoint = 'wss://h0j3zwoo.api.satori.com';
+        var appkey = 'd3fE5A8bc1D9C2e8761DfCf7d6cab13a';
+        
+        var client = new RTM(endpoint, appkey);
+        //window.open('http://www.google.com','self');
+        client.on('enter-connected', function () {
+          console.log('Connected to Satori RTM!');
+        });
+        
+        client.on('error', function (error) {
+          console.log('Failed to connect', error);
+        });
+        
+        var channel = client.subscribe('output', RTM.SubscriptionMode.SIMPLE);
+        
+        /* set callback for state transition */
+        channel.on('enter-subscribed', function () {
+          console.log('Subscribed to: ' + channel.subscriptionId);
+          //window.open('http://www.google.com','self');
+        });
+        
+        channel.on('leave-subscribed', function () {
+          console.log('Unsubscribed from: ' + channel.subscriptionId);
+        });
+        
+        /* set callback for PDU with specific action */
+        channel.on('rtm/subscription/data', function (pdu) {
+          pdu.body.messages.forEach(function (msg) {
+            console.log('Got animal ' + msg.who + ': ' + JSON.stringify(msg));
+            window.open("https://www.google.com/maps/dir/?api=1&origin=draper+university&destination=san+francisco",'_system');
+          });
+        });
+        
+        channel.on('rtm/subscribe/error', function (pdu) {
+          console.log('Failed to subscribe. RTM replied with the error ' +
+              pdu.body.error + ': ' + pdu.body.reason);
+        });
+        
+        channel.on('rtm/subscription/error', function (pdu) {
+          console.log('Subscription failed. RTM sent the unsolicited error ' +
+              pdu.body.error + ': ' + pdu.body.reason);
+        });
+        
+        client.start();
+            
+    };
+
+}])
+   
+.controller('page10Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
